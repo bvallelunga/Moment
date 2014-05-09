76172161556d42c6be54dd06f0381f3ee5d74836
Brian Vallelunga <vallelungabrian@gmail.com>
2014-05-08 23:11:09 -0700

    added native ios

---
+++ ios/Moment/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Moment
+//
+//  Created by Brian Vallelunga on 5/8/14.
+//  Copyright (c) 2014 Brian Vallelunga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
